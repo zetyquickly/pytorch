b61613d1381b9cb6b2da9d2fe2f31d828e87aaa8
Ivan Kobzarev <ivankobzarev@fb.com>
2020-04-22 21:03:18 -0700

    [mgpu][integr][2/n] Vulkan/GL Integration prototype, applying review comments

--- binaries/vulkan_torch.cc
+++ binaries/vulkan_torch.cc
@@ -9,11 +9,7 @@
 #include "torch/csrc/jit/serialization/import.h"
 #include "torch/script.h"
 
-#define FLF __FILE__ << ":" << __LINE__ << " " << __FUNCTION__
-#define COUT_FLF std::cout << FLF << std::endl;
-
 void test_to_from_vulkan() {
-  COUT_FLF;
   auto tcpu = torch::tensor(
       {{
 
@@ -26,46 +22,29 @@ void test_to_from_vulkan() {
   std::cout << "tcpu:" << tcpu << std::endl;
   std::cout << "tcpu.options():" << tcpu.options() << std::endl;
 
-  auto tcpus = tcpu.as_strided({1, 2, 1, 2}, {1, 1, 2, 2}, 0); 
+  auto tcpus = tcpu.as_strided({1, 2, 1, 2}, {1, 1, 2, 2}, 0);
   std::cout << "tcpus:" << tcpus << std::endl;
   std::cout << "tcpus.options():" << tcpus.options() << std::endl;
 
   std::cout << "===> tcpu.to_vulkan()" << std::endl;
-  COUT_FLF;
   auto tv = tcpu.to_vulkan();
-  COUT_FLF;
 
-
-  
   std::cout << "tv.device():" << tv.device() << std::endl;
-  COUT_FLF;
   std::cout << "tv.layout():" << tv.layout() << std::endl;
-  COUT_FLF;
   std::cout << "tv.options():" << tv.options() << std::endl;
-  
-  
-  
+
   std::cout << "===> tv.as_strided()" << std::endl;
-  auto tvs = tv.as_strided({1, 2, 1, 2}, {1, 1, 2, 2}, 0); 
-  COUT_FLF;
+  auto tvs = tv.as_strided({1, 2, 1, 2}, {1, 1, 2, 2}, 0);
   std::cout << "tvs:" << tvs << std::endl;
-  COUT_FLF;
   std::cout << "tvs.options():" << tvs.options() << std::endl;
-  COUT_FLF;
-  
-  
-  
-  
+
   std::cout << "===> tv.to_densea()" << std::endl;
-  COUT_FLF;
   auto tcpuout = tv.to_dense();
-  COUT_FLF;
   std::cout << "tcpuout:" << tcpuout << std::endl;
   std::cout << "tcpuout.options():" << tcpuout.options() << std::endl;
 }
 
 void test_upsample_nearest2d() {
-  COUT_FLF;
   auto tcpu = torch::tensor(
       {{
 
@@ -75,27 +54,13 @@ void test_upsample_nearest2d() {
 
       }},
       torch::kFloat);
-  std::cout << "tcpu:" << tcpu << std::endl;
-
   auto tv = tcpu.to_vulkan();
-  COUT_FLF;
-  std::cout << "tv.device():" << tv.device() << std::endl;
-
-  COUT_FLF;
   auto tvout = at::upsample_nearest2d(tv, {4, 6});
-
-  COUT_FLF;
-  std::cout << "tvout.device():" << tvout.device() << std::endl;
-
-  COUT_FLF;
   auto tcpuout = tvout.to_dense();
-
-  COUT_FLF;
   std::cout << "tcpuout:" << tcpuout << std::endl;
 }
 
 void test_add() {
-  COUT_FLF;
   auto tin0_cpu = torch::tensor(
       {{
 
@@ -115,28 +80,16 @@ void test_add() {
       }},
       torch::kFloat);
 
-  std::cout << "tin0_cpu:" << tin0_cpu << std::endl;
-  std::cout << "tin1_cpu:" << tin1_cpu << std::endl;
-
   auto tout_cpu_expected = at::add(tin0_cpu, tin1_cpu, 2.f);
   std::cout << "tout_cpu_expected:" << tout_cpu_expected << std::endl;
-
   auto tin0_v = tin0_cpu.to_vulkan();
-  COUT_FLF;
   auto tin1_v = tin1_cpu.to_vulkan();
-
-  COUT_FLF;
   auto tout_v = at::add(tin0_v, tin1_v, 2.f);
-
-  COUT_FLF;
   auto tout_cpu = tout_v.to_dense();
-
-  COUT_FLF;
   std::cout << "tout_cpu:" << tout_cpu << std::endl;
 }
 
 void test_conv() {
-  COUT_FLF;
   auto tin_cpu = torch::tensor( // 1, 3, 3, 3
       {{
           // c_0
@@ -198,11 +151,6 @@ void test_conv() {
       },
       torch::kFloat);
   auto tb_cpu = torch::tensor({0, 0}, torch::kFloat);
-
-  std::cout << "tin_cpu: " << tin_cpu << std::endl;
-  std::cout << "tw: " << tw_cpu << std::endl;
-  std::cout << "tb: " << tb_cpu << std::endl;
-
   int64_t groups = 1;
 
   auto tout_cpu_expected = at::conv2d(
@@ -213,13 +161,8 @@ void test_conv() {
       c10::IntArrayRef{0}, // padding
       c10::IntArrayRef{1}, // dilation
       groups);
-
   std::cout << "tout_cpu_expected:" << tout_cpu_expected << std::endl;
-
-  COUT_FLF;
   auto tin_v = tin_cpu.to_vulkan();
-
-  COUT_FLF;
   auto tout_v = at::conv2d(
       tin_v,
       tw_cpu,
@@ -228,24 +171,14 @@ void test_conv() {
       {0}, // padding
       {1}, // dilation,
       groups);
-
-  COUT_FLF;
   auto tout_cpu = tout_v.to_dense();
-  COUT_FLF;
   std::cout << "tout_cpu:" << tout_cpu << std::endl;
 }
 
 int main(int argc, char** argv) {
-  // COUT_FLF;
-  // test_upsample_nearest2d();
-
-  // COUT_FLF;
-  // test_add();
-
-  // COUT_FLF;
-  // test_conv();
-
+  test_upsample_nearest2d();
+  test_add();
+  test_conv();
   test_to_from_vulkan();
-
   return 0;
 }
